402bf3299901b4f9ef217472aa40a026b3edc9c0
Eran Kampf <eran@ekampf.com>
2019-02-06 14:47:49 -0800

    nicer code

--- examples/simple/client.py
+++ examples/simple/client.py
@@ -1,13 +1,13 @@
 # coding: gql
 
-MY_QUERY = gql"""
-query hello_query {
+QUERY = gql"""
+query HelloQuery {
   hello(argument: "World")
 }
 """
 
 def main():
-    result = MY_QUERY.execute()
+    result = QUERY.execute()
     print(result)
 
 if __name__ == "__main__":
