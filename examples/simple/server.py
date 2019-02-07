c342f0415924f65825f3d13bd35993264f149c48
Eran Kampf <eran@ekampf.com>
2019-02-06 17:53:34 -0800

    lint

--- examples/simple/server.py
+++ examples/simple/server.py
@@ -3,13 +3,14 @@ from flask_graphql import GraphQLView
 from flask import Flask
 
 class Query(graphene.ObjectType):
+    # pylint:disable=no-self-use
     hello = graphene.String(argument=graphene.String(default_value="stranger"))
 
-    def resolve_hello(self, info, argument):
+    def resolve_hello(self, _, argument):
         return 'Hello ' + argument
 
-schema = graphene.Schema(query=Query)
+SCHEMA = graphene.Schema(query=Query)
 
 
-app = Flask(__name__)
-app.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True))
+APP = Flask(__name__)
+APP.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql', schema=SCHEMA, graphiql=True))
